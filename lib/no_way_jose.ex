581618709e72a31b9625810829a7811c09073ae5
Sonny Scroggin <sonny@scrogg.in>
2019-10-13 12:50:41 -0500

    Add some typespecs and docs

--- lib/no_way_jose.ex
+++ lib/no_way_jose.ex
@@ -1,4 +1,19 @@
 defmodule NoWayJose do
+  @moduledoc """
+  Provides functions for signing a map of "claims" into a JWT using
+  a signing key.
+  """
+
+  @type claims :: %{binary() => term()}
+  @type signer :: binary()
+  @type token :: binary()
+
+  @doc """
+  Generates a signed JWT from the given claims and signer.
+
+  Returns a JWT on success and raises an error on error.
+  """
+  @spec sign!(claims(), signer()) :: token() | no_return()
   def sign!(claims, signer) do
     case sign(claims, signer) do
       {:ok, token} -> token
@@ -6,6 +21,10 @@ defmodule NoWayJose do
     end
   end
 
+  @doc """
+  Generates a signed JWT from the given claims and signer.
+  """
+  @spec sign(claims(), signer()) :: {:ok, token()} | {:error, term()}
   def sign(claims, signer) do
     NoWayJose.Native.sign(claims, signer)
   end
