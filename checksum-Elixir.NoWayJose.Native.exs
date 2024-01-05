5bf684e5fa205dacd62ddddcdba0c0a389f1e982
Sonny Scroggin <sonny@scrogg.in>
2024-01-05 11:23:39 -0600

    Add checksum

---
+++ checksum-Elixir.NoWayJose.Native.exs
@@ -0,0 +1,11 @@
+%{
+  "libnowayjose-v0.3.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:181c6a69cd464fdd9ae7024ddaf7b0b85fe8ee55df21a7847a82ef3bac02ebdd",
+  "libnowayjose-v0.3.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:bf277d59e32525fef036fa6305260c9af869ed1f4d44361daf3e1abd31e372b5",
+  "libnowayjose-v0.3.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:d4accdd9360121ef6362460626d3238ed85b0debcbaefdc0a95703e61b363eb5",
+  "libnowayjose-v0.3.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:5a799aca6018bed25ce0a2a359dd12b202fcb4c6ba4432df4b943a71a632b12b",
+  "libnowayjose-v0.3.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:7dbd7712a9d91d1cf00a2eea6ed93cd125242cdcabbac88c29a69afa39fcaeec",
+  "libnowayjose-v0.3.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:273e5c63375b833969421cb2809461ea8c04a250f585ed074b86e5c44160ad34",
+  "libnowayjose-v0.3.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:189daec4bae4e2879a9b3e01a875babdd6d6e1987d88f5833e4e82a1b9579c35",
+  "nowayjose-v0.3.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:81552559c8945001a4a53ac636ba4629eca3a4329e1e5d880f411d2fafb6face",
+  "nowayjose-v0.3.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:d3df015cce13b3efdcbf8c4d53d90055279cc66e12d76a23a169d7e31a9baf92",
+}
