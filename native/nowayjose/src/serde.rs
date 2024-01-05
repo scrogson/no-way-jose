5ea7d34daec1e40ada9f46009ecb9e73efd5f605
Sonny Scroggin <sonny@scrogg.in>
2024-01-05 10:23:19 -0600

    Remove openssl and use rsa directly

--- native/nowayjose/src/serde.rs
+++ native/nowayjose/src/serde.rs
@@ -3,6 +3,8 @@ mod error;
 mod ser;
 mod utils;
 
+#[allow(unused_imports)]
 pub use de::{from_term, Deserializer};
 pub use error::Error;
+#[allow(unused_imports)]
 pub use ser::{to_term, Serializer};
